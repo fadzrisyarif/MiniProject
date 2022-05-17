0ae30aa8ea78e289530c4eb1a6c8cadafbb9c61e
Fadzri Syarif <fadzrisyarif@gmail.com>
2022-05-17 19:38:43 +0700

    finish attemp 6

--- dryerr/src/components/Header.js
+++ dryerr/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Container, Row, Col} from 'react-bootstrap'
+import {Container, Row, Col, Button} from 'react-bootstrap'
 import SendOrderModal from './SendOrderModal'
 
 function Header() {
@@ -11,8 +11,9 @@ function Header() {
       <Container>
                 <Row style={{display:'flex', alignItems:'center', paddingBottom: '2rem'}}>
                     <Col >
-                        <h2 className='pb-5'>Dryerrrr Steam Machine automatic dryer after cleaning Clothes. Dryer as well as Sterilizing and Hanging Clothes Irons.</h2>
-                        <SendOrderModal/>
+                        <h2 className='pb-5'>Dryerr Steam Machine automatic dryer after cleaning Clothes. Dryer as well as Sterilizing and Hanging Clothes Irons.</h2>
+                        {/* <SendOrderModal/> */}
+                        <Button href='https://invoice-staging.xendit.co/od/dryerr-pay' variant='success'>Order Now</Button>
                     </Col>
                     <Col>
                     <img src='dryerr1.png'/>
