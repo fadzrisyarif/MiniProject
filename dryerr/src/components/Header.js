3e3e3b1038348679a3a92f2ca73c591772bb156e
Fadzri Syarif <fadzrisyarif@gmail.com>
2022-05-20 21:59:15 +0700

    finish attemp 8

--- dryerr/src/components/Header.js
+++ dryerr/src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
                     <Col >
                         <h2 className='pb-5' style={{color:'whitesmoke'}}>Dryerr Steam Machine automatic dryer after cleaning Clothes. Dryer as well as Sterilizing and Hanging Clothes Irons.</h2>
                         {/* <SendOrderModal/> */}
-                        <button className='outer-order' href='https://invoice-staging.xendit.co/od/dryerr-pay' variant='success'><img className='order-btn' src={orderbtn}></img></button>
+                        <Button className='outer-order' href='https://invoice-staging.xendit.co/od/dryerr-pay'><img className='order-btn' src={orderbtn}></img></Button>
                     </Col>
                     <Col>
                     <img src={dryerr1}/>
